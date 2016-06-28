cb99f2252ae7dca81fb245496eab4b923de80a9b
sdesimeur <pro@sdesimeur.com>
2016-06-28 18:00:29 +0200

    modify url to GoogleMaps

--- gmap2xxx/View.js
+++ gmap2xxx/View.js
@@ -253,7 +253,7 @@ function View () {
 
 $(document).ready(function () { 
     var view = new View();
-    document.body.innerHTML=document.body.innerHTML.replace(/###GMAPLNK###/g,'<a href="https://www.google.fr/maps" target="_blank">GoogleMaps</a>');
+    document.body.innerHTML=document.body.innerHTML.replace(/###GMAPLNK###/g,'<a href="https://www.google.fr/maps/dir/" target="_blank">GoogleMaps</a>');
     document.body.innerHTML=document.body.innerHTML.replace(/###BMAPLNK###/g,'<a href="https://www.bing.com/mapspreview" target="_blank">BingMaps</a>');
     view.loadall();
     
