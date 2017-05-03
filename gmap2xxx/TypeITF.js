d4498eaf8558d3ba7f70b78ac6bd388d7d92fcda
sam <sam@sdesimeur.com>
2017-05-03 14:09:21 +0200

    take care of toll in TypeITF (p4)

--- gmap2xxx/TypeITF.js
+++ gmap2xxx/TypeITF.js
@@ -56,11 +56,11 @@ function TypeITF (tabs,myCaller) {
                 if (i<obj.nbetapes-1) {
                     dv.setUint32(etape1offset+i*uneEtapeLength+lonstartoffset,lng,this.littleEndian);
                     dv.setUint32(etape1offset+i*uneEtapeLength+lonstartoffset+deltalon,lat,this.littleEndian);
-                    dv.setUint32(etape1offset+i*uneEtapeLength+highwayAndTollOffset,highwayAndTollValue,this.littleEndian);
                 }
                 if (i>0) {
                     dv.setUint32(etape1offset+(i-1)*uneEtapeLength+lonendoffset,lng,this.littleEndian);
                     dv.setUint32(etape1offset+(i-1)*uneEtapeLength+lonendoffset+deltalon,lat,this.littleEndian);
+                    dv.setUint32(etape1offset+(i-1)*uneEtapeLength+highwayAndTollOffset,highwayAndTollValue,this.littleEndian);
                 }
                 i++;
             }
